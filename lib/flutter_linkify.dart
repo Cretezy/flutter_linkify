9adaaddfaccbb3c78180258860ec5409e4503b71
Charles Crete <charles@cretezy.com>
2023-05-15 19:02:57 -0400

    Merge #104 and #97

--- lib/flutter_linkify.dart
+++ lib/flutter_linkify.dart
@@ -52,7 +52,7 @@ class Linkify extends StatelessWidget {
   final int? maxLines;
 
   /// How visual overflow should be handled.
-  final TextOverflow overflow;
+  final TextOverflow overflow?;
 
   /// The number of font pixels for each logical pixel
   final double textScaleFactor;
